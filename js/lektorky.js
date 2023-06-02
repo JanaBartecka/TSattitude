9356ff1b829f9886380d709298779951ed817cae
Jana Bartecka <xcrazygirl@seznam.cz>
2023-06-02 13:15:56 +0200

    20230602 zaloha3

--- js/lektorky.js
+++ js/lektorky.js
@@ -6,13 +6,13 @@ allLektorky.forEach((lektorka)=> {
             event.target.parentNode.childNodes[3].classList.remove('lektorky__name--active')
             event.target.parentNode.childNodes[5].classList.remove('lektorky__plus--active')
             event.target.parentNode.childNodes[7].classList.remove('lektorky__description--active')
-            //event.target.parentNode.childNodes[1].classList.remove('lektorky__item--hover')
+            event.target.parentNode.childNodes[1].classList.remove('lektorky__item--hover')
         } else {
             event.target.parentNode.childNodes[1].classList.add('lektorky__item--active')
             event.target.parentNode.childNodes[3].classList.add('lektorky__name--active')
             event.target.parentNode.childNodes[5].classList.add('lektorky__plus--active')
             event.target.parentNode.childNodes[7].classList.add('lektorky__description--active')
-            //event.target.parentNode.childNodes[1].classList.remove('lektorky__item--hover')
+            event.target.parentNode.childNodes[1].classList.remove('lektorky__item--hover')
         }
     })
     lektorka.addEventListener('mouseenter',()=>{
