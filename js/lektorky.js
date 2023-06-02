b6b02a7e64848245b8a537b89ff226fcc2ded8c9
Jana Bartecka <xcrazygirl@seznam.cz>
2023-06-02 13:08:48 +0200

    20230602 zaloha3

--- js/lektorky.js
+++ js/lektorky.js
@@ -1,17 +1,25 @@
 let allLektorky=document.querySelectorAll('.lektorky__lektorka')
 allLektorky.forEach((lektorka)=> {
     lektorka.addEventListener('click',function(event){
-        event.target.parentNode.childNodes[1].classList.toggle('lektorky__item--active')
-        event.target.parentNode.childNodes[3].classList.toggle('lektorky__name--active')
-        event.target.parentNode.childNodes[5].classList.toggle('lektorky__plus--active')
-        event.target.parentNode.childNodes[7].classList.toggle('lektorky__description--active')
-        //event.target.parentNode.childNodes[1].classList.remove('lektorky__item--hover')
+        if (event.target.parentNode.childNodes[1].classList.contains('lektorky__item--active')){
+            event.target.parentNode.childNodes[1].classList.remove('lektorky__item--active')
+            event.target.parentNode.childNodes[3].classList.remove('lektorky__name--active')
+            event.target.parentNode.childNodes[5].classList.remove('lektorky__plus--active')
+            event.target.parentNode.childNodes[7].classList.remove('lektorky__description--active')
+            //event.target.parentNode.childNodes[1].classList.remove('lektorky__item--hover')
+        } else {
+            event.target.parentNode.childNodes[1].classList.add('lektorky__item--active')
+            event.target.parentNode.childNodes[3].classList.add('lektorky__name--active')
+            event.target.parentNode.childNodes[5].classList.add('lektorky__plus--active')
+            event.target.parentNode.childNodes[7].classList.add('lektorky__description--active')
+            //event.target.parentNode.childNodes[1].classList.remove('lektorky__item--hover')
+        }
     })
     lektorka.addEventListener('mouseenter',()=>{
-        lektorka.firstElementChild.classList.toggle('lektorky__item--hover')
+        lektorka.firstElementChild.classList.add('lektorky__item--hover')
     })
     lektorka.addEventListener('mouseout',()=>{
-        lektorka.firstElementChild.classList.toggle('lektorky__item--hover')
+        lektorka.firstElementChild.classList.remove('lektorky__item--hover')
     })
 })
 
