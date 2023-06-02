f96c7c8b788ac2769cc0d018c53e8ad34cb747d9
Jana Bartecka <xcrazygirl@seznam.cz>
2023-06-02 11:28:29 +0200

    20230602 zaloha1

--- js/menu.js
+++ js/menu.js
@@ -35,8 +35,7 @@ function updateActiveMenuItem() {
   
       // Check if section is in the viewport
       if (
-        section.offsetTop <= currentScrollPos &&
-        section.offsetTop + section.offsetHeight > currentScrollPos
+        section.offsetTop <= currentScrollPos && section.offsetTop + section.offsetHeight > currentScrollPos
       ) {
         // Add 'active' class to the menu link
         link.classList.add('menu__link--active');
@@ -52,11 +51,38 @@ function updateActiveMenuItem() {
   // Attach scroll event listener to the window object
   window.addEventListener('scroll', updateActiveMenuItem);
 
+  const MenuIcons=document.querySelectorAll('.menu__icon')
+  console.log(MenuIcons);
+  const BodyElement=document.querySelector('body')
+  const ComputedStyleBody=window.getComputedStyle(BodyElement,null)
+  const DefaultFontSize=ComputedStyleBody.getPropertyValue('font-size')
+  const DefaultFontSizeValue=DefaultFontSize.split('px');
+  //resize menu list to fit along with menu__icon
   window.addEventListener('scroll',()=> {
-    if (window.scrollY > 100) {
-      MenuList.classList.add('menu__list--full')
+    if (window.innerWidth>DefaultFontSizeValue[0]*73.5) {
+      if (window.scrollY > 100) {
+        MenuList.classList.add('menu__list--full')
+        MenuIcons.forEach((icon)=> {
+          icon.classList.remove('menu__icon--full')
+        })
+      } else {
+        MenuList.classList.remove('menu__list--full')
+        MenuIcons.forEach((icon)=> {
+          icon.classList.add('menu__icon--full')
+        })
+      }
+    } 
+  })
+
+  window.addEventListener('resize', ()=> {
+    if (window.innerWidth>DefaultFontSizeValue[0]*73.5) {
+      MenuIcons.forEach((icon)=> {
+        icon.classList.add('menu__icon--full')
+      })
     } else {
-      MenuList.classList.remove('menu__list--full')
+      MenuIcons.forEach((icon)=> {
+        icon.classList.remove('menu__icon--full')
+      })
     }
   })
 
